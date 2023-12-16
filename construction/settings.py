e55824e291f67e0ae2744fe872e64e1e6707908d
Emmanuel1 <Nnamdiemmanuel01@gmail.com>
2023-12-16 10:14:01 +0100

    new

--- construction/settings.py
+++ construction/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8)x+-+o$xotgn#u52*m)l*9^l+w=z6jl-ix%4ye&*+ky3v99$f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lebtech1-c5c411ab5e01.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
